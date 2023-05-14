91dc62cfca5ece00fb89093179003b11f83d66be
CharlesGameDev <99226468+CharlesGameDev@users.noreply.github.com>
2023-05-14 02:59:35 -0400

    1.0.4 (small fixes)

--- js/index.js
+++ js/index.js
@@ -2,201 +2,241 @@ CHARMS = {
     "compass": {
         "name": "Wayward Compass",
         "desc": "Whispers its location to the bearer whenever a map is open, allowing wanderers to pinpoint their current location.",
+        "effect": "Location awareness",
         "cost": 1
     },
     "swarm": {
         "name": "Gathering Swarm",
         "desc": "A swarm will follow the bearer and gather up any loose Geo.<br><br>Useful for those who can't bear to leave anything behind, no matter how insignificant.",
+        "effect": "Geo collection",
         "cost": 1
     },
     "stalwart": {
         "name": "Stalwart Shell",
         "desc": "Builds resilience. When recovering from damage, the bearer will remain invulnerable for longer.<br><br>Makes it easier to escape from dangerous situations.",
+        "effect": "Longer i-frames",
         "cost": 2
     },
     "soulcatcher": {
         "name": "Soul Catcher",
         "desc": "Used by shamans to draw more SOUL from the world around them.<br><br>Increases the amount of SOUL gained when striking an enemy with the nail.",
+        "effect": "+3 soul per hit",
         "cost": 2
     },
     "shaman": {
         "name": "Shaman Stone",
         "desc": "Said to contain the knowledge of past generations of shaman.&lt;br&gt;&lt;br&gt;Increases the power of spells, dealing more damage to foes.",
+        "effect": "33-51% stronger spells",
         "cost": 3
     },
     "souleater": {
         "name": "Soul Eater",
         "desc": "Forgotten shaman artifact, used to draw SOUL from still-living creatures.&lt;br&gt;&lt;br&gt;Greatly increases the amount of SOUL gained when striking an enemy with the nail.",
+        "effect": "+8 soul per hit",
         "cost": 4
     },
     "dashmaster": {
         "name": "Dashmaster",
         "desc": "Bears the likeness of an eccentric bug known only as &#39;The Dashmaster&#39;.&lt;br&gt;&lt;br&gt;The bearer will be able to dash more often as well as dash downwards. Perfect for those who want to move around as quickly as possible.",
+        "effect": "33% faster dashes",
         "cost": 2
     },
     "sprintmaster": {
         "name": "Sprintmaster",
         "desc": "Bears the likeness of a strange bug known only as &#39;The Sprintmaster&#39;.&lt;br&gt;&lt;br&gt;Increases the running speed of the bearer, allowing them to avoid danger or overtake rivals.",
+        "effect": "20% faster movement",
         "cost": 1
     },
     "grubsong": {
         "name": "Grubsong",
         "desc": "Contains the gratitude of freed grubs.&lt;br&gt;&lt;br&gt;Gain SOUL when taking damage.",
+        "effect": "+15 soul when taking damage",
         "cost": 1
     },
     "elegy": {
         "name": "Grubberfly's Elegy",
         "desc": "Contains the gratitude of grubs who will move to the next stage of their lives. Imbues weapons with a holy strength.&lt;br&gt;&lt;br&gt;When the bearer is at full health, they will fire beams of white-hot energy from their nail.",
+        "effect": "",
         "cost": 3
     },
     "heart": {
         "name": "Unbreakable Heart",
         "desc": "Increases the health of the bearer, allowing them to take more damage.&lt;br&gt;&lt;br&gt;This charm is unbreakable.",
+        "effect": "+2 masks",
         "cost": 2
     },
     "greed": {
         "name": "Unbreakable Greed",
         "desc": "Causes the bearer to find more Geo when defeating enemies.&lt;br&gt;&lt;br&gt;This charm is unbreakable.",
+        "effect": "20-100% more Geo from enemies",
         "cost": 2
     },
     "strength": {
         "name": "Unbreakable Strength",
         "desc": "Strengthens the bearer, increasing the damage they deal to enemies with their nail.&lt;br&gt;&lt;br&gt;This charm is unbreakable.",
+        "effect": "50% more nail damage",
         "cost": 3
     },
     "twister": {
         "name": "Spell Twister",
         "desc": "Reflecting the desires of the Soul Sanctum for mastery over SOUL, it improves the bearer&#39;s ability to cast spells.&lt;br&gt;&lt;br&gt;Reduces the SOUL cost of casting spells.",
+        "effect": "-9 spell soul cost",
         "cost": 2
     },
     "steadybody": {
         "name": "Steady Body",
         "desc": "Keeps its bearer from recoiling backwards when they strike an enemy with a nail.&lt;br&gt;&lt;br&gt;Allows one to stay steady and keep attacking.",
+        "effect": "Removes nail knockback",
         "cost": 1
     },
     "heavyblow": {
         "name": "Heavy Blow",
         "desc": "Formed from the nails of fallen warriors.&lt;br&gt;&lt;br&gt;Increases the force of the bearer&#39;s nail, causing enemies to recoil further when hit.",
+        "effect": "75% more nail knockback",
         "cost": 2
     },
     "quickslash": {
         "name": "Quick Slash",
         "desc": "Born from imperfect, discarded nails that have fused together. The nails still long to be wielded.&lt;br&gt;&lt;br&gt;Allows the bearer to slash much more rapidly with their nail.",
+        "effect": "39% faster nail swings",
         "cost": 3
     },
     "longnail": {
         "name": "Longnail",
         "desc": "Increases the range of the bearer&#39;s nail, allowing them to strike foes from further away.",
+        "effect": "15% more nail range",
         "cost": 2
     },
     "markofpride": {
         "name": "Mark of Pride",
         "desc": "Freely given by the Mantis Tribe to those they respect.&lt;br&gt;&lt;br&gt;Greatly increases the range of the bearer&#39;s nail, allowing them to strike foes from further away.",
+        "effect": "25% more nail range",
         "cost": 3
     },
     "fury": {
         "name": "Fury of the Fallen",
         "desc": "Embodies the fury and heroism that comes upon those who are about to die.&lt;br&gt;&lt;br&gt;When close to death, the bearer&#39;s strength will increase.",
+        "effect": "Deals double damage when on one mask",
         "cost": 2
     },
     "thorns": {
         "name": "Thorns of Agony",
         "desc": "Senses the pain of its bearer and lashes out at the world around them.&lt;br&gt;&lt;br&gt;When taking damage, sprout thorny vines that damage nearby foes.",
+        "effect": "",
         "cost": 1
     },
     "baldurshell": {
         "name": "Baldur Shell",
         "desc": "Protects its bearer with a hard shell while focusing SOUL.<br><br>The shell is not indestructible and will shatter if it absorbs too much damage.",
+        "effect": "",
         "cost": 2
     },
     "flukenest": {
         "name": "Flukenest",
         "desc": "Living charm born in the gut of a Flukemarm.<br><br>Transforms the Vengeful Spirit spell into a horde of volatile baby flukes.",
+        "effect": "Transform Vengeful Spirit into flukes",
         "cost": 3
     },
     "dcrest": {
         "name": "Defender's Crest",
         "desc": "Unique charm bestowed by the King of Hallownest to his most loyal knight. Scratched and dirty, but still cared for.<br><br>Causes the bearer to emit a heroic odour.",
+        "effect": "",
         "cost": 1
     },
     "glowingwomb": {
         "name": "Glowing Womb",
         "desc": "Drains the SOUL of its bearer and uses it to birth hatchlings.<br><br>The hatchlings have no desire to eat or live, and will sacrifice themselves to protect their parent.",
+        "effect": "",
         "cost": 2
     },
     "quickfocus": {
         "name": "Quick Focus",
         "desc": "A charm containing a crystal lens.<br><br>Increases the speed of focusing SOUL, allowing the bearer to heal damage faster.",
+        "effect": "33% faster focus time",
         "cost": 3
     },
     "deepfocus": {
         "name": "Deep Focus",
         "desc": "Naturally formed within a crystal over a long period. Draws in SOUL from the surrounding air.<br><br>The bearer will focus SOUL at a slower rate, but the healing effect will double.",
+        "effect": "Focus heals 2 masks<br>65% longer focus time",
         "cost": 4
     },
     "lifebloodheart": {
         "name": "Lifeblood Heart",
         "desc": "Contains a living core that seeps precious lifeblood.<br><br>When resting, the bearer will gain a coating of lifeblood that protects from a modest amount of damage.",
+        "effect": "+2 lifeblood masks",
         "cost": 2
     },
     "lifebloodcore": {
         "name": "Lifeblood Core",
         "desc": "Contains a living core that bleeds precious lifeblood.<br><br>When resting, the bearer will gain a coating of lifeblood that protects from a large amount of damage.",
+        "effect": "+4 lifeblood masks",
         "cost": 3
     },
     "joni": {
         "name": "Joni's Blessing",
         "desc": "Blessed by Joni, the kindly heretic. Transfigures vital fluids into blue lifeblood.<br><br>The bearer will have a healthier shell and can take more damage, but they will not be able to heal themselves by focusing SOUL.",
+        "effect": "No healing<br>40% more masks<br>All masks are lifeblood",
         "cost": 4
     },
     "hiveblood": {
         "name": "Hiveblood",
         "desc": "Golden nugget of the Hive's precious hardened nectar.<br><br>Heals the bearer's wounds over time, allowing them to regain health without focusing SOUL.",
+        "effect": "Heals the last mask lost after 10 seconds",
         "cost": 4
     },
     "spore": {
         "name": "Spore Shroom",
         "desc": "Composed of living fungal matter. Scatters spores when exposed to SOUL.<br><br>When focusing SOUL, emit a spore cloud that slowly damages enemies.",
+        "effect": "",
         "cost": 1
     },
     "sharpshadow": {
         "name": "Sharp Shadow",
         "desc": "Contains a forbidden spell that transforms shadows into deadly weapons.<br><br>When using Shadow Dash, the bearer's body will sharpen and damage enemies.",
+        "effect": "40% longer shadow dash<br>Shadow dashes deal 1x nail damage",
         "cost": 2
     },
     "unn": {
         "name": "Shape of Unn",
         "desc": "Reveals the form of Unn within the bearer.<br><br>While focusing SOUL, the bearer will take on a new shape and can move freely to avoid enemies.",
+        "effect": "",
         "cost": 2
     },
     "nmg": {
         "name": "Nailmaster's Glory",
         "desc": "Contains the passion, skill and regrets of a Nailmaster.<br><br>Increases the bearer's mastery of Nail Arts, allowing them to focus their power faster and unleash arts sooner.",
+        "effect": "56% faster Nail Art charge time",
         "cost": 1
     },
     "weaversong": {
         "name": "Weaversong",
         "desc": "Silken charm containing a song of farewell, left by the Weavers who departed Hallownest for their old home.<br><br>Summons weaverlings to give the lonely bearer some companionship and protection.",
+        "effect": "",
         "cost": 2
     },
     "dreamwielder": {
         "name": "Dream Wielder",
         "desc": "Transient charm created for those who wield the Dream Nail and collect Essence.<br><br>Allows the bearer to charge the Dream Nail faster and collect more SOUL when striking foes.",
+        "effect": "+33 soul when using the Dream Nail<br>62.5% faster Dream Nail charge time",
         "cost": 1
     },
     "dreamshield": {
         "name": "Dreamshield",
         "desc": "Defensive charm once wielded by a tribe that could shape dreams.<br><br>Conjures a shield that follows the bearer and attempts to protect them.",
+        "effect": "",
         "cost": 3
     },
     "grimmchild": {
         "name": "Grimmchild",
         "desc": "Worn by those who take part in the Grimm Troupe's Ritual.<br><br>The bearer must seek the Grimmkin and collect their flames. Uncollected flames will appear on the bearer's map.",
+        "effect": "",
         "cost": 2
     },
     "carefreemelody": {
         "name": "Carefree Melody",
         "desc": "Token commemorating the start of a friendship.<br><br>Contains a song of protection that may defend the bearer from damage.",
+        "effect": "~22.46% chance to block a hit",
         "cost": 3
     }
 }
@@ -230,6 +270,7 @@ window.onload = function() {
     COST_NOTCHES = document.getElementById("COST_NOTCHES");
     CHARM_IMAGE = document.getElementById("CHARM_IMAGE");
     DESCRIPTION_TEXT = document.getElementById("DESCRIPTION_TEXT");
+    EFFECTS_TEXT = document.getElementById("EFFECTS_TEXT");
 
     RIGHTSIDE = document.getElementById("rightside");
 
@@ -263,7 +304,7 @@ window.onload = function() {
         }
     });
 
-    hover_charm("");
+    hover_charm("compass");
     update_screen();
 }
 
@@ -271,6 +312,7 @@ function update_screen()
 {
     set_notches();
     set_charms();
+    // set_effects();
     create_extra_notch();
     update_notches();
     update_charm_list();
@@ -302,7 +344,25 @@ function set_charms()
 {
     CHARMS_DIV.innerHTML = "";
     EQUIPPED_CHARMS.forEach(charm_name => {
-        equip_charm(charm_name);
+        equip_charm(charm_name)
+    });
+    EQUIPPED_CHARMS.splice().reverse().forEach(charm_name => {
+        const charm = CHARMS[charm_name];
+        console.log(charm_name + ": " + charm.cost + " | " + total_notch_cost())
+        if (charm.cost + (total_notch_cost() - charm.cost) > NOTCHES)
+        {
+            remove_charm(charm_name);
+        }
+    })
+}
+
+function set_effects()
+{
+    EFFECTS_TEXT.innerHTML = "";
+    EQUIPPED_CHARMS.forEach(charm_name => {
+        const effect = CHARMS[charm_name].effect;
+        if (effect.trim() == "") return;
+        EFFECTS_TEXT.innerHTML += effect + "<br>";
     });
 }
 
@@ -451,7 +511,6 @@ function change_notches()
 function sound(sound_name)
 {
     set_cookie("sound_enabled", SOUND_INPUT.checked);
-    console.log(SOUND_INPUT.checked);
     if (!SOUND_INPUT.checked) return;
     const audio = new Audio(`sfx/${sound_name}.wav`);
     audio.volume = 0.4;
